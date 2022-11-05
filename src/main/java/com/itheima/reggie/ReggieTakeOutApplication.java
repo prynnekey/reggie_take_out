040eaa1905db8e9d3126e86e506f23a4287c5bfd
prynnekey <prynnekey@gmail.com>
2022-11-05 15:31:54 +0800

    新增：Redis环境配置

--- src/main/java/com/itheima/reggie/ReggieTakeOutApplication.java
+++ src/main/java/com/itheima/reggie/ReggieTakeOutApplication.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @author prynn
+ */
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
